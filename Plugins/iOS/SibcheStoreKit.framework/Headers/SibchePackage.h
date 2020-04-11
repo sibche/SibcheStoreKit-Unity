1818201704034bca923a5bddb6824f2e313e4a73
Mehdi Hosseinzadeh <mehdi.hosseinzadeh86@gmail.com>
2020-04-11 15:30:42 +0430

    Updated SibcheStoreKit to 4.1.1

---
+++ Plugins/iOS/SibcheStoreKit.framework/Headers/SibchePackage.h
@@ -0,0 +1,31 @@
+//
+//  SibchePackage.h
+//  SibcheStoreKit
+//
+//  Created by Mehdi on 4/13/19.
+//  Copyright © 2019 Sibche. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SibchePackage : NSObject
+
+- (NSString*)packageId;
+- (NSString*)type;
+- (NSString*)code;
+- (NSString*)name;
+- (NSString*)packageDescription;
+- (NSNumber*)price;
+- (NSNumber*)totalPrice;
+- (NSNumber*)discount;
+
+- (NSString*)toJson;
+- (NSDictionary*)toDictionary;
+
+- (instancetype)initWithData:(NSDictionary*)data;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Plugins/iOS/SibcheStoreKit.framework/Headers/SibcheSubscriptionPackage.h
@@ -0,0 +1,25 @@
+//
+//  SibcheSubscriptionPackage.h
+//  SibcheStoreKit
+//
+//  Created by Mehdi on 4/13/19.
+//  Copyright © 2019 Sibche. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SibchePackage.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SibcheSubscriptionPackage : SibchePackage
+
+- (NSNumber*)duration;
+- (NSString*)group;
+
+- (instancetype)initWithData:(NSDictionary*)data;
+- (NSString *)toJson;
+- (NSDictionary*)toDictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
